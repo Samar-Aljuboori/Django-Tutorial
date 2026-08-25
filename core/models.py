8e4096c0b0bf204f778e23594fdc5d7bcfc6b60a
Samar-Aljuboori <smsm119.outlook.de>
2026-08-25 11:15:32 +0200

    Link Task model to Django User model

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone  # Import Django's timezone utility
-# Task model to store user to-do items
+                                   # Task model to store user to-do items
+from django.contrib.auth.models import User   # addd user login/ Authentication
+
 class Task(models.Model):   # creat table
     title = models.CharField(max_length=200)      # create first column
     completed = models.BooleanField(default=False)      # create second column
@@ -10,13 +12,18 @@ class Task(models.Model):   # creat table
         ('low', 'Low'),
     ]
 
-    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='low')
-    due_date = models.DateField(null=True, blank=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='low')   # 4th column
+    due_date = models.DateField(null=True, blank=True)                       # 5th column
     @property
     def is_overdue(self):
         if self.due_date and not self.completed:
             return self.due_date < timezone.now().date()
         return False
 
+     # @property is a built-in Python decorator that turns this method into a dynamic attribute.
+     # It allows accessing 'task.is_overdue' without parentheses (), which is required for Django HTML templates.
+   
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # 6th column 
+   
     def __str__(self):            # The actual task name will appear.
         return self.title
